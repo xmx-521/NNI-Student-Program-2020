48230d1081aac61a2d27db0cea850c1bf7956443
oliver_shang <olivershang0017@gmail.com>
2020-12-20 22:12:33 +0800

    feat(task4):更新preprocess.py

--- Task4/code/utils/preprocess.py
+++ Task4/code/utils/preprocess.py
@@ -1,4 +1,18 @@
 import numpy as np
 import h5py
 
-f = h5py.File("test.hfd5", "w")
+train_or_test = 'test' #set it to train/test to generate train/test_dataset.hfd5 files
+f = h5py.File(train_or_test + "_dataset.hfd5", "w")
+data_path = './'+ train_or_test+ '/'
+
+#convert raw data to hfd5 file
+for label in range(21):
+    grp = f.create_group(str(label),"w")
+    for i in range(100):
+        dataset = grp.create_dataset(str(i+1), (128,128,2), dtype = 'i')
+        _data = np.loadtxt(data_path + str(label) + '/' + str(i+1) + '.txt')
+        for j in range(len(_data)):
+            if _data[i, 3] == 0:
+                dataset[_data[i, 1],_data[i, 2], 0] += 1
+            else:
+                dataset[_data[i, 1], _data[i, 2], 1] += 1
